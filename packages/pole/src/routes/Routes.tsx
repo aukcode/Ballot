7dc2fb212c87bb509780856016c63ba63b62f652
aukcode <oyvind.auk@gmail.com>
2019-09-02 07:44:52 +0200

    Revert "feat: add login module"
    This reverts commit d9906aae76cb10c8592b6e5cabe93abb3190a158.

--- packages/pole/src/routes/Routes.tsx
+++ packages/pole/src/routes/Routes.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { RouteMap } from './RouteMap';
 import { HomeComponent } from './home/Home';
-import { LoginComponent } from './login/Login';
 
 export const Routes = () => {
   return (
@@ -13,11 +12,8 @@ export const Routes = () => {
           component={HomeComponent}
           exact={true}
         />
-        <Route
-          path={RouteMap.login.path}
-          component={LoginComponent}
-          exact={true}
-        />
+
+        <Route />
       </Switch>
     </BrowserRouter>
   );
