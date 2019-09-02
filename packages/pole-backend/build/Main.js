424b0942427ea931038cf97b4e9fa9845fef085f
aukcode <oyvind.auk@gmail.com>
2019-09-02 08:20:09 +0200

    feat: add post function and jwt

--- packages/pole-backend/build/Main.js
+++ packages/pole-backend/build/Main.js
@@ -6,6 +6,7 @@ var __importDefault =
   };
 Object.defineProperty(exports, '__esModule', { value: true });
 const express_1 = __importDefault(require('express'));
+const jwt = require('jsonwebtoken');
 const app = express_1.default();
 const port = 8080;
 const hey = 'Hello from pole-backend';
@@ -15,5 +16,24 @@ const whiteListDomains = (req, res, next) => {
 };
 app.use(whiteListDomains);
 app.get('/hey', (req, res) => res.json({ hey }).send());
+app.get('/api', (req, res) =>
+  res
+    .json({
+      message: 'Welcome to the API now',
+    })
+    .send()
+);
+app.post('/api/login', (req, res) => {
+  const user = {
+    id: 1,
+    username: 'auk',
+    email: 'auk@gmail.com',
+  };
+  jwt.sign({ user }, 'secretkey', (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 //# sourceMappingURL=Main.js.map
--- packages/pole-backend/src/Main.ts
+++ packages/pole-backend/src/Main.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = 8080;
 
@@ -18,5 +19,27 @@ const whiteListDomains = (req: Request, res: Response, next: NextFunction) => {
 app.use(whiteListDomains);
 
 app.get('/hey', (req: Request, res: Response) => res.json({ hey }).send());
+
+app.get('/api', (req: Request, res: Response) =>
+  res
+    .json({
+      message: 'Welcome to the API now',
+    })
+    .send()
+);
+
+app.post('/api/login', (req: Request, res: Response) => {
+  // Mock user
+  const user = {
+    id: 1,
+    username: 'auk',
+    email: 'auk@gmail.com',
+  };
+  jwt.sign({ user }, 'secretkey', (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
