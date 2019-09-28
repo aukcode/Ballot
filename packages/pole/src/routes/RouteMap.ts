3b0dc36e74ba886775f2bcfc4e2550a8bd266935
aukcode <oyvind.auk@gmail.com>
2019-09-28 18:56:25 +0200

    make create site

--- packages/pole/src/routes/RouteMap.ts
+++ packages/pole/src/routes/RouteMap.ts
@@ -2,7 +2,14 @@ export const RouteMap = {
   home: {
     path: '/',
   },
+  pole: {
+    path: '/pole',
+  },
   login: {
     path: '/login',
+  },
+  create: {
+    path: '/create',
+    new: '/create/:id',
   },
 };
--- packages/pole/src/routes/Routes.tsx
+++ packages/pole/src/routes/Routes.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { RouteMap } from './RouteMap';
 import { HomeComponent } from './home/Home';
+import { PoleComponent } from './pole/Pole';
+import { CreateComponent } from './create/Create';
 
 export const Routes = () => {
   return (
@@ -12,7 +14,16 @@ export const Routes = () => {
           component={HomeComponent}
           exact={true}
         />
-
+        <Route
+          path={RouteMap.pole.path}
+          component={PoleComponent}
+          exact={true}
+        />
+        <Route
+          path={RouteMap.create.path}
+          component={CreateComponent}
+          exact={true}
+        />
         <Route />
       </Switch>
     </BrowserRouter>
