ad389e08701d469ddd32c0a99b50db82eeef2825
aukcode <oyvind.auk@gmail.com>
2019-11-03 18:36:46 +0100

    remove unused code

--- packages/ballot-web/src/routes/home/Home.tsx
+++ packages/ballot-web/src/routes/home/Home.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { withRouter } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { ChangeEvent } from 'react';
-import { RouteMap } from '../RouteMap';
 
 interface HomeProps {}
 
