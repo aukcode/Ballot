2f6f4a72711e13c65553f9bfd3ac6e8166dabe10
aukcode <oyvind.auk@gmail.com>
2019-11-03 19:43:46 +0100

    iterate styling

--- packages/ballot-web/src/routes/home/Home.tsx
+++ packages/ballot-web/src/routes/home/Home.tsx
@@ -26,36 +26,34 @@ const Home = (props: HomeProps) => {
         </p>
       </div>
 
-      <div className="max-w-sm mx-auto mt-48 flex justify-center p-6 bg-white rounded-lg shadow-xl">
-        <div className="pt-1">
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4 mt-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="pin"
-              >
-                Join a Poll!
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                id="pin"
-                placeholder="Poll Pin"
-                value={pin}
-                onChange={handlePinChange}
-              />
-            </div>
+      <div className="max-w-sm mx-auto mt-48">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4 mt-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="pin"
+            >
+              Join a Poll!
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              id="pin"
+              placeholder="Poll Pin"
+              value={pin}
+              onChange={handlePinChange}
+            />
+          </div>
 
-            <div className="flex items-center justify-between">
-              <button
-                className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit"
-              >
-                ENTER
-              </button>
-            </div>
-          </form>
-        </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              ENTER
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
