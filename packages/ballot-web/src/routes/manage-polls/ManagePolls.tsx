487b1dd58cb298cd0d3f38d791caa6934eb32021
aukcode <oyvind.auk@gmail.com>
2019-11-09 21:12:00 +0100

    add manage component

---
+++ packages/ballot-web/src/routes/manage-polls/ManagePolls.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { Poll } from '../../models/Poll';
+import { QuestionCard } from './poll-card/QuestionCard';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+
+interface CreateComponentRouterProps {}
+
+interface CreateComponentOwnProps {}
+
+type CreateComponentProps = RouteComponentProps<CreateComponentRouterProps> &
+  CreateComponentOwnProps;
+
+const ManagePolls = (props: CreateComponentProps) => {
+  const mockPoll: Poll = {
+    id: 'aldnføaehsrpoaw4borqnøo3url',
+    pollPin: 123456,
+    title: 'Test Poll',
+    questions: [
+      {
+        id: 'ldkjfhasldfa',
+        question: "Who's your daddy",
+        options: ['Me', 'Myself', 'I'],
+      },
+    ],
+  };
+
+  const mockPolls = [];
+  for (let i = 0; i < 5; i++) {
+    mockPolls.push(mockPoll);
+  }
+
+  return (
+    <div>
+      <div className="header-my-polls">
+        <div className="header-my-polls"></div>
+        <div>
+          {mockPolls.map(poll => (
+            <div>
+              <div>{poll.title}</div>
+              <div>{poll.questions.map(q => q.question)}</div>
+            </div>
+          ))}
+        </div>
+        <h1>My Finalized Polls</h1>
+        map finished polls
+      </div>
+    </div>
+  );
+};
+
+export const ManagePollsComponent = withRouter(ManagePolls);
