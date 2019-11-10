06b98b5bc9330f0b4c1bd08a135710dfe5fd4c67
aukcode <oyvind.auk@gmail.com>
2019-11-10 16:24:30 +0100

    render polls and archived polls

--- packages/ballot-web/src/routes/manage/ManagePolls.tsx
+++ packages/ballot-web/src/routes/manage/ManagePolls.tsx
@@ -1,49 +1,100 @@
 import * as React from 'react';
-import { Poll } from '../../models/Poll';
-import { QuestionCard } from './cards/QuestionCard';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Poll } from '../../models/Poll';
+import { useAuth } from '../../api/auth/AuthContext';
+import { PollCard } from './cards/PollCard';
 
-interface CreateComponentRouterProps {}
+enum ErrorMessage {
+  USER_NOT_FOUND = 'User not found. Are you sure you are logged in?',
+  SERVER_ERROR = 'There was an error connecting to the server',
+}
 
-interface CreateComponentOwnProps {}
+interface ManagePollsComponentRouterProps {}
 
-type CreateComponentProps = RouteComponentProps<CreateComponentRouterProps> &
-  CreateComponentOwnProps;
+interface ManagePollsComponentOwnProps {}
 
-const ManagePolls = (props: CreateComponentProps) => {
-  const mockPoll: Poll = {
-    id: 'aldnføaehsrpoaw4borqnøo3url',
-    pollPin: 123456,
-    active: false,
-    title: 'Test Poll',
-    questions: [
-      {
-        id: 'ldkjfhasldfa',
-        question: "Who's your daddy",
-        options: ['Me', 'Myself', 'I'],
-      },
-    ],
+type Props = RouteComponentProps<ManagePollsComponentRouterProps> &
+  ManagePollsComponentOwnProps;
+
+const ManagePolls = (props: Props) => {
+  const { user } = useAuth();
+  const [polls, setPolls] = useState<Poll[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  useEffect(() => {
+    if (user) {
+      const userId = user.id;
+      try {
+        const fetchPolls = async () => {
+          const result = await fetch(
+            `http://localhost:8080/api/polls/user/${userId}`,
+            {
+              method: 'GET',
+            }
+          );
+          result
+            .json()
+            .then(res => {
+              return setPolls(res);
+            })
+            .catch(err =>
+              setErrorMessage(`${ErrorMessage.SERVER_ERROR}: ${err}`)
+            );
+        };
+        fetchPolls();
+      } catch (err) {
+        setErrorMessage(`${ErrorMessage.SERVER_ERROR}: ${err}`);
+      }
+    } else {
+      setErrorMessage(`${ErrorMessage.USER_NOT_FOUND}`);
+    }
+  }, []);
+
+  const renderPolls = () => {
+    if (polls.length > 0) {
+      return (
+        <div>
+          {polls
+            .filter(poll => !poll.archived)
+            .map(poll => {
+              return <PollCard poll={poll} />;
+            })}
+        </div>
+      );
+    } else {
+      return (
+        <p className="font-semibold text-gray-600">
+          You don't have any polls yet. Click the New Poll button to start!
+        </p>
+      );
+    }
   };
 
-  const mockPolls = [];
-  for (let i = 0; i < 5; i++) {
-    mockPolls.push(mockPoll);
-  }
+  const renderArchivedPolls = () => {
+    const archivedPolls = polls.filter(poll => poll.archived);
+    if (archivedPolls.length > 0) {
+      return (
+        <div>
+          <h1 className="text-4xl border-b-2">Archived Polls</h1>
+          {polls
+            .filter(poll => poll.archived)
+            .map(poll => {
+              return <PollCard poll={poll} />;
+            })}
+        </div>
+      );
+    }
+  };
 
   return (
-    <div>
-      <div className="header-my-polls">
-        <div className="header-my-polls"></div>
-        <div>
-          {mockPolls.map(poll => (
-            <div>
-              <div>{poll.title}</div>
-              <div>{poll.questions.map(q => q.question)}</div>
-            </div>
-          ))}
-        </div>
-        <h1>My Finalized Polls</h1>
-        map finished polls
+    <div className="flex justify-center mt-8">
+      <div className="mx-3 w-full sm:max-w-3xl p-4">
+        <h1 className="text-4xl border-b-2">Your Polls</h1>
+        <p className="font-bold text-red-500 mt-2">{errorMessage}</p>
+        <div className="my-4">{renderPolls()}</div>
+
+        <div className="my-4">{renderArchivedPolls()}</div>
       </div>
     </div>
   );
