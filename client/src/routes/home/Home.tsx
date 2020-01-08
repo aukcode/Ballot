02c10493ea8d575ad7e7995d10025f83e43bedbf
Øyvind Aukner <oyvind.auk@gmail.com>
2020-01-08 11:45:51 +0100

    Add desired to text

--- client/src/routes/home/Home.tsx
+++ client/src/routes/home/Home.tsx
@@ -42,7 +42,7 @@ const Home = (props: Props) => {
               className="block text-gray-700 text-sm font-bold mb-2"
               htmlFor="name"
             >
-              Enter your name
+              Enter your desired name
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
