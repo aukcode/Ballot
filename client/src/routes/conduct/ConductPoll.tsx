98ecbf0ab01c8e94eebdbe56e44b2eecd4b820ad
aukoyy <oyvind.auk@gmail.com>
2020-02-12 17:44:40 +0100

    add fetch function

--- client/src/routes/conduct/ConductPoll.tsx
+++ client/src/routes/conduct/ConductPoll.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { backendAddress } from '../../config';
+import { Question } from '../../models/Question';
+
+enum ErrorMessage {
+    QUESTION_NOT_FOUND = 'Question not found',
+    USER_NOT_FOUND = 'User not found. Are you sure you are logged in?',
+    SERVER_ERROR = 'There was an error connecting to the server',
+  }
 
 interface ConductPollComponentRouterProps {
     pollId: string;
@@ -11,8 +20,54 @@ type props = RouteComponentProps<ConductPollComponentRouterProps> &
 ConductPollComponentOwnProps;
 
 const ConductPoll = (props: props) => {
+    const [pollTitle, setPollTitle] = useState<string>('');
+    const [questions, setQuestions] = useState<Question[]>([]);
+    const [errorMessage, setErrorMessage] = useState<string>('');
+    const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+
+    const pollId = props.match.params.pollId;
+
+    useEffect(() => {
+          try {
+            const fetchPoll = async () => {
+              const result = await fetch(
+                `${backendAddress}/api/polls/${pollId}`,
+                {
+                  method: 'GET',
+                }
+              );
+              result
+                .json()
+                .then(res => {
+                  setQuestions(res.questions);
+                  console.log(res.questions)
+                  setPollTitle(res.title);
+                })
+                .catch(err =>
+                  setErrorMessage(`${ErrorMessage.QUESTION_NOT_FOUND}: ${err}`)
+                );
+            };
+            fetchPoll();
+          } catch (err) {
+            setErrorMessage(`${ErrorMessage.SERVER_ERROR}: ${err}`);
+          }
+      }, [pollId]);
+
+      const renderCurrentQuestion = () => {
+          return (
+              <div>
+                  <p>{questions[0].question}</p>
+              </div>
+          )
+      }
+
+
     return (
-        <div>lol {props.match.params.pollId}</div>
+        <div className="flex justify-center mt-8">
+            <p className="text-2xl">{pollTitle}</p>
+            {questions.map(question => question.question)}
+            
+        </div>
     )
 };
 
