b69c5479be6881e117a2ab65836638425bb4bfb7
aukoyy <oyvind.auk@gmail.com>
2020-03-25 10:07:05 +0100

    add http endpoint new

--- server/routes/ActivePolls.ts
+++ server/routes/ActivePolls.ts
@@ -3,8 +3,23 @@ import { Request, Response } from 'express';
 const ActivePoll = require('../schemas/ActivePoll');
 import v4 = require('uuid/v4');
 
-// new
+// new. There should be controls to only have one activePoll/ actual poll
+router.post('/new', async (req: Request, res: Response) => {
+  const newActivePoll = new ActivePoll({
+    pollId: req.body.pollId,
+    currentQuestion: 0,
+  });
 
-// update
+  try {
+    await newActivePoll.save();
+    res.status(200).send(newActivePoll);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// patch. frontend controls what is updated
 
 // delete
+
+module.exports = router;
